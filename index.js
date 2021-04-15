fb80db80601b2c4fe5c87034405b45d0514bd023
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-04-15 10:18:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const fi = (function() {
+// Filtering to get Total Sum of Car Prices
+const topVehicles = [
+  { make: 'Ford', model: 'F-150', type: 'truck', price: 28940 },
+  { make: 'Ford', model: 'Fusion', type: 'sedan', price: 23170 },
+  { make: 'Honda', model: 'CR-V', type: 'suv', price: 25350 },
+  { make: 'Honda', model: 'Accord', type: 'sedan', price: 24970 },
+  { make: 'Toyota', model: 'Rav4', type: 'suv', price: 26250 },
+  { make: 'Toyota', model: 'Tundra', type: 'truck', price: 33825 },
+  { make: 'Chevrolet', model: 'Equinox', type: 'suv', price: 23800 },
+  { make: 'Chevrolet', model: 'Malibu', type: 'sedan', price: 22140 },
+  { make: 'Nissan', model: 'Rouge', type: 'suv', price: 25750 },
+  { make: 'Nissan', model: 'Titan', type: 'truck', price: 36650 }
+];
+
+fi = (function() {
   return {
     libraryMethod: function() {
-      return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
+      return "reading the article!";
     },
 
     each: function() {
-      //code here
-    },
+const totalSum = 
+  topVehicles.filter(c => c.type === 'suv'),
+  topVehicles.map(c => c.price),
+  c.price => c.price.reduce((x,y) => x + y, 0)
+) (topVehicles);
 
-    map: function() {
-      //code here
-    },
+console.log(totalSum);
+      
+// Adds New Array Property
+topVehicles.forEach(c => {
+ c['size'] = "large";
+ if (c.type == 'suv'){
+   c['size'] = "medium occupancy";
+ }
+ if (c.type == 'truck'){
+   car['size'] = "small occupancy";
+ }
+});
+}
+  };
+})();
 
-    reduce: function() {
-      //code here
-    },
-
-    functions: function() {
-       //code here
-    },
+fi.libraryMethod();
 
 
-  }
-})()
-
-fi.libraryMethod()
